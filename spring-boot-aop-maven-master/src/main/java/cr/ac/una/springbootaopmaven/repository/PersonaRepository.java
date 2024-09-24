905957a1ead8f58cddf73407f7357a199b9d4a78
Roger Cascante <rogercascante17@gmail.com>
2024-09-23 22:22:15 -0600

    limpiando repo

--- spring-boot-aop-maven-master/src/main/java/cr/ac/una/springbootaopmaven/repository/PersonaRepository.java
+++
@@ -1,8 +0,0 @@
-package cr.ac.una.springbootaopmaven.repository;
-
-import cr.ac.una.springbootaopmaven.entity.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository< Persona, Long> {
-
-}
